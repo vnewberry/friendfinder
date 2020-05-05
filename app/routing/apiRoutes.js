f3f4e16750a50ea4e575100eb7da108df55d9eda
Vance Newberry <vancenewberry@gmail.com>
2020-05-04 23:22:54 -0500

    nearly done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,63 +1,50 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
 var friends = require("../data/friends");
-// var waitListData = require("../data/waitinglistData");
-
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-//   app.get("/api/waitlist", function(req, res) {
-//     res.json(waitListData);
-//   });
+
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-//   app.post("/api/friends", function(req, res) {
-//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-//     // It will do this by sending out the value "true" have a table
-//     // req.body is available since we're using the body parsing middleware
-//     if (tableData.length < 5) {
-//       tableData.push(req.body);
-//       res.json(true);
-//     }
-//     else {
-//       waitListData.push(req.body);
-//       res.json(false);
-//     }
-//   });
+  app.post("/api/friends", function(req, res) {
+var newScores = req.body.scores;
+var scoreArr = [];
+var friendCount = 0;
+var match = 0;
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+for (i=0;i<friends.length;i++){
+    var difference = 0;
+    for(j=0;j<newScores.length;j++){
+        difference += (Math.abs(parseInt(friends[i].scores[j])-parseInt(newScores[j])));
+    }
+    scoreArr.push(difference);
+}
 
-//   app.post("/api/clear", function(req, res) {
-//     // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
+for (i=0;i<scoreArr.length;i++){
+    if(scoreArr[i] <= scoreArr[match]){
+        match = i;
+    }
+}
 
-//     res.json({ ok: true });
-//   });
+var bestFriend = friends[match];
+res.json(bestFriend);
+
+//pushes new submission into the friendsList array
+friends.push(req.body);
+});
 };
+
+
